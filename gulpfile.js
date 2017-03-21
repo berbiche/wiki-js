f751be4c972594af0de1694043a3221d67d00101
Berbiche <e1567925@cmaisonneuve.qc.ca>
2017-03-21 11:09:18 -0400

    Optimised the tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,29 @@
 "use strict";
-const spawn  = require('child_process').spawn;
-const del    = require('del');
-const fs     = require('fs');
-const gulp   = require('gulp-help')(require('gulp'), {'hideEmpty': true});
-const concat = require('gulp-concat');
+const spawn   = require('child_process').spawn;
+const del     = require('del');
+const fs      = require('fs');
+const flatten = require('gulp-flatten');
+const gulp    = require('gulp-help')(require('gulp'), {'hideEmpty': true});
+// const concat = require('gulp-concat');
 // const debug  = require('gulp-debug');
-const inject = require('gulp-inject');
-const sass   = require('gulp-sass');
-const ts     = require('gulp-typescript');
-const gutil  = require('gulp-util');
-const path   = require('path');
-const run    = require('run-sequence');
+// const inject = require('gulp-inject');
+const sass    = require('gulp-sass');
+const ts      = require('gulp-typescript');
+const gutil   = require('gulp-util');
+const newer   = require('gulp-newer');
+const path    = require('path');
+const run     = require('run-sequence');
 
 const tsSrc    = ts.createProject('src/tsconfig.json');
 const tsServer = ts.createProject('server/tsconfig.json');
 const colors   = gutil.colors;
 
 /////////////////////////
-/*     Clean tasks     */
+/*     Clean task      */
 /////////////////////////
 // Clean up the dist folder
 gulp.task('clean', 'Clean the dist directory of its content', () => {
-    return del(['dist'], {'ignore': 'dist/libs'});
-});
-// Clean the css only
-gulp.task('clean:css', false, () => {
-    return del(['dist/src/app/base.css']);
-});
-// Clean the html
-gulp.task('clean:html', false, () => {
-    return del(['dist/src/**/*.html']);
-});
-// Clean the client javascript
-gulp.task('clean:js', false, () => {
-    return del(['dist/src/**/*.js']);
-});
-// Clean the server
-gulp.task('clean:server', false, () => {
-    return del(['dist/server']);
+    return del(['dist'], { 'ignore': ['dist', 'dist/libs'] });
 });
 
 /////////////////////////
@@ -45,17 +31,21 @@ gulp.task('clean:server', false, () => {
 /////////////////////////
 // Compile the typescript code of src
 gulp.task('compile:js', false, () => {
-    let result = tsSrc.src()
+    const dest = 'dist/src';
+    let result = gulp.src('src/**/*.ts')
+        .pipe(newer(dest, { 'ext': '.js' }))
         .pipe(tsSrc());
-    return result.js.pipe(gulp.dest('dist/src'));
+    return result.js.pipe(gulp.dest(dest));
 });
 // Compile the typescript for the server to dist/server
 gulp.task('compile:server', false, () => {
+    const dest = 'dist/server';
     let result = gulp.src('server/**/*.ts')
+        .pipe(newer(dest, { 'ext': '.js' }))
         .pipe(tsServer());
-    return result.js.pipe(gulp.dest('dist/server'));
+    return result.js.pipe(gulp.dest(dest));
 });
-// Compile the sass to dist/src/app.css
+// Compile the sass to dist/src/base.css
 gulp.task('compile:sass', false, () => {
     return gulp.src('src/styles/base.scss')
         .pipe(sass.sync({'outputStyle': 'compressed'}))
@@ -77,43 +67,23 @@ gulp.task('copy:lib', false, () => {
         'ui-router-ng2/_bundles/ui-router-ng2.min.js',
         'zone.js/dist/zone.min.js',
         'core-js/client/core.min.js',
-        'systemjs/dist/system-production.js',
+        'systemjs/dist/system.js',
         'rxjs/bundles/Rx.min.js'
     ], { 'cwd': 'node_modules/**' })
+    .pipe(flatten())
     .pipe(gulp.dest('dist/lib/'));
 });
-// Copy the html files to dist/src
+// Copy the html files to dist/src/**/
 gulp.task('copy:html', false, () => {
+    const dest = 'dist/src';
     return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist/src'));
-});
-// Inject javascript libraries in index.html
-gulp.task('inject:lib', false, ['copy:lib'], () => {
-    let target = gulp.src('./dist/src/index.html');
-    let source = gulp.src('./dist/lib/**/*.js', {'read': false});
-    return target
-        .pipe(inject(source, {'starttag': '<!-- inject:lib -->'}))
-        .pipe(gulp.dest('dist/src'));
+        .pipe(newer(dest))
+        .pipe(gulp.dest(dest));
 });
 
 /////////////////////////
-/*     Build tasks     */
+/*      Build task     */
 /////////////////////////
-// Replace the css in dist with newly compiled css
-gulp.task('build:sass', false, (cb) => {
-    run('clean:css', 'compile:sass', cb);
-});
-// Replace the javascript with the new javascript of src
-gulp.task('build:js', false, (cb) => {
-    run('clean:js', 'compile:js', cb);
-});
-// Replace the html with the new html of src
-gulp.task('build:html', false, (cb) => {
-    run('clean:html', 'copy:html', 'inject:lib', cb);
-});
-gulp.task('build:server', false, (cb) => {
-    run('clean:server', 'compile:server', 'run', cb);
-});
 // Build the project
 gulp.task('build', 'Build the application', (cb) => {
     run('clean', [
@@ -123,7 +93,7 @@ gulp.task('build', 'Build the application', (cb) => {
             'copy:lib',
             'copy:html'
         ],
-        'inject:lib', 'run', cb);
+        'run', cb);
 });
 
 /////////////////////////
@@ -131,10 +101,10 @@ gulp.task('build', 'Build the application', (cb) => {
 /////////////////////////
 // Watch for changes
 gulp.task('watch', false, () => {
-    gulp.watch('src/**/*.scss', ['build:sass']);
-    gulp.watch('src/**/*.ts', ['build:js']);
-    gulp.watch('src/**/*.html', ['build:html']);
-    gulp.watch('server/**/*.ts', () => { run('build:server', 'watch'); });
+    gulp.watch('src/**/*.scss', ['compile:sass']);
+    gulp.watch('src/**/*.ts', ['compile:js']);
+    gulp.watch('src/**/*.html', ['copy:html']);
+    gulp.watch('server/**/*.ts', () => { run('compile:server', 'run'); });
 });
 
 /////////////////////////
@@ -144,23 +114,33 @@ gulp.task('watch', false, () => {
 gulp.task('run', 'Run the application and watch for changes', (cb) => {
     run('run:express', 'watch', cb);
 });
-// The instance of express server
+
+
+// The instance of the Express server
 let server = undefined;
-let serverTimer = undefined;
+// starts the server and handles process exit
 gulp.task('run:express', false, () => {
     if (server === undefined) {
-        gutil.log(colors.green('[server] ') + 'Starting');
         start();
     } else {
         restart();
     }
 
+    // On process exit, kill the server
+    process.on('exit', (code) => {
+        if (server) {
+            server.kill('SIGKILL');
+            removeEvent();
+        }
+        gutil.log(colors.green('Closed the process with code ') + colors.red(`${code}`));
+    });
+
     // Event handlers
     function stdout(data) {
-        gutil.log(colors.green('[server] ') + colors.blue(data));
+        gutil.log(colors.green('[server] ') + colors.blue(typeof data === 'string' ? data.trim() : data));
     }
     function stderr(data) {
-        gutil.log(colors.green('[server] ') + colors.red(data));
+        gutil.log(colors.green('[server] ') + colors.red(typeof data === 'string' ? data.trim() : data));
     }
     function error(err) {
         if (err instanceof Error)
@@ -172,7 +152,6 @@ gulp.task('run:express', false, () => {
         gutil.log(colors.green('[server] ') +
             'exited with: ' +
             colors.red('[code => %d, sig => %s]'), code, sig);
-        serverTimer && clearTimeout(serverTimer);
         removeEvent();
         server = undefined;
         //if the error code or sig is not null exit the process
@@ -184,10 +163,14 @@ gulp.task('run:express', false, () => {
 
     function start() {
         // create a child_process running the express server
+        gutil.log(colors.green('[server] ') + 'Starting');
         server = spawn(
             'node',
             ['dist/server/wiki.js'],
-            {'env': process.env}
+            {
+                'env': process.env,
+                'cwd': __dirname
+            }
         );
         setEvent();
     }
@@ -203,11 +186,8 @@ gulp.task('run:express', false, () => {
         // kill the child process
         if (server) {
             server.kill('SIGTERM');
-            serverTimer = setTimeout(() => {
-                server.kill('SIGKILL'); //die fucker
-                removeEvent();
-                server = undefined;
-            }, 2000);
+            removeEvent();
+            server = undefined;
         }
     }
 
