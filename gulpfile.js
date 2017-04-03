b98f285f6d8618230d7f1a40c876baf486d2a10a
Nicolas Berbiche <nic.berbiche@gmail.com>
2017-04-02 21:47:19 -0400

    Upgraded Gulp Tasks in advance for Gulp 4 TODO: Implement a way to let gulp know the task has finished when the server closes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,226 +1,190 @@
 "use strict";
-const spawn   = require('child_process').spawn;
+const fork    = require('child_process').fork;
 const del     = require('del');
 const fs      = require('fs');
+const gulp    = require('gulp');
 const flatten = require('gulp-flatten');
-const gulp    = require('gulp-help')(require('gulp'), {'hideEmpty': true});
-// const concat = require('gulp-concat');
-// const debug  = require('gulp-debug');
-// const inject = require('gulp-inject');
 const sass    = require('gulp-sass');
 const ts      = require('gulp-typescript');
 const gutil   = require('gulp-util');
-const newer   = require('gulp-newer');
 const merge   = require('merge-stream');
-const path    = require('path');
-const run     = require('run-sequence');
-
-const tsSrc    = ts.createProject('src/tsconfig.json');
-const tsServer = ts.createProject('server/tsconfig.json');
-const chalk    = gutil.colors;
-
-// The instance of the Express server
-// Used to kill the process
-let server = undefined;
+const config  = require('./config.json');
+const chalk   = gutil.colors;
 
 /////////////////////////
-/*        Other        */
+/*        Tasks        */
 /////////////////////////
-// On process exit, kill the server
-process.once('exit', exit);
-// Same as above
-process.once('SIGINT', exit);
-// Same as above
-process.once('SIGBREAK', exit);
+const compile = gulp.parallel(compile_js, compile_sass, compile_server);
+compile.description = 'Compile the source files of the application';
 
-function exit(code) {
-    server && server.kill('SIGKILL');
-    code && gutil.log('Exiting process with code ' + chalk.red(`${code}`));
-    process.exit();
+const copy = gulp.parallel(copy_html, copy_lib);
+copy.description = 'Copy libraries and html files to the server directory';
+
+const build = gulp.series(clean, gulp.parallel(compile, copy));
+build.description = 'Build the project';
+
+const run_watch = gulp.parallel(run_express, watch);
+run_watch.description = 'Start the Express server and watch for changes to files';
+
+clean.description = 'Clean the dist directory of its content except for lib';
+function clean() {
+    return del(['dist/*', '!dist/lib', '!dist/src/favicon.ico']);
 }
 
-/////////////////////////
-/*     Clean task      */
-/////////////////////////
-// Clean up the dist folder
-gulp.task('clean', 'Clean the dist directory of its content', () => {
-    return del(['dist/*', '!dist/lib']);
-});
+gulp.task('build', build);
+gulp.task('clean', clean);
+gulp.task('compile', compile);
+gulp.task('copy', copy);
+gulp.task('run', run_express);
+gulp.task('run:watch', run_watch);
 
 /////////////////////////
-/*    Compile tasks    */
+/*      Variables      */
+/////////////////////////
+// The instance of the Express server, used to kill the child process
+const tsClientSrc = ts.createProject('./src/tsconfig.json');
+const tsServerSrc = ts.createProject('./server/tsconfig.json');
+const startTime   = Date.now();
+// maps the output.locations with output.prefix to a new object
+const outputDir   = Object.entries(config.output.locations)
+    .reduce((obj, [key, value]) =>
+        Object.defineProperty(obj, key, {'value': `${config.output.prefix}/${value}`})
+    , {});
+
+/////////////////////////
+/*      Functions      */
 /////////////////////////
 // Compile the typescript code of src
-gulp.task('compile:js', false, () => {
-    const dest = 'dist/src';
-    let result = gulp.src('src/**/*.ts')
-        .pipe(newer(dest, { 'ext': '.js' }))
-        .pipe(tsSrc());
-    return result.js.pipe(gulp.dest(dest));
-});
+function compile_js() {
+    return (fs.existsSync(outputDir.client + '/app')
+        ? gulp.src('src/**/*.ts', { 'since': startTime })
+        : gulp.src('src/**/*.ts'))
+        .pipe(tsClientSrc())
+        .js.pipe(gulp.dest(outputDir.client));
+}
+
 // Compile the typescript for the server to dist/server
-gulp.task('compile:server', false, () => {
-    const dest = 'dist/server';
-    let result = gulp.src('server/**/*.ts')
-        .pipe(newer(dest, { 'ext': '.js' }))
-        .pipe(tsServer());
-    return result.js.pipe(gulp.dest(dest));
-});
+function compile_server() {
+    return (fs.existsSync(outputDir.server)
+        ? gulp.src('server/**/*.ts', { 'since': startTime })
+        : gulp.src('server/**/*.ts'))
+        .pipe(tsServerSrc())
+        .js.pipe(gulp.dest(outputDir.server));
+};
+
 // Compile the sass to dist/src/base.css
-gulp.task('compile:sass', false, () => {
+function compile_sass() {
     return gulp.src('src/styles/base.scss')
         .pipe(sass.sync({'outputStyle': 'compressed'}))
         .pipe(gulp.dest('dist/src/css'));
-});
+};
 
-/////////////////////////
-/*      Copy tasks     */
-/////////////////////////
 // Copy the required libraries to dist/lib
-gulp.task('copy:lib', false, () => {
-    if (fs.existsSync('dist/lib')) {
+function copy_lib(cb) {
+    if (fs.existsSync(outputDir.libraries)) {
         gutil.log(`Skipping ${chalk.cyan("'copy:lib'")} -> `+
             `${chalk.green("'dist/lib'")} already exists`);
         cb();
+        return;
     }
 
-    let tmp1 = gulp.src([
-        '@angular/**/bundles/*.min.js',
-        '@angular/material/bundles/*.js',
-        '@angular/material/core/theming/prebuilt/deeppurple-amber.css',
-        'ui-router-ng2/_bundles/ui-router-ng2.min.js',
-        'reflect-metadata/Reflect.js',
-        'zone.js/dist/zone.min.js',
-        'core-js/client/core.min.js',
-        'systemjs/dist/system.js'
-    ], { 'cwd': 'node_modules/**' })
-    .pipe(flatten())
-    .pipe(gulp.dest('dist/lib/'));
-
-    let tmp2 = gulp.src('rxjs/**/*.js', { 'cwd': 'node_modules/**' })
-        .pipe(gulp.dest('dist/lib/'));
-
+    let tmp1 = gulp.src('node_modules/rxjs/**/*.js', { 'base': 'node_modules' })
+        .pipe(gulp.dest('./dist/lib'));
+    let tmp2 = gulp.src([
+            '@angular/**/bundles/*.min.js',
+            '@angular/material/bundles/*.js',
+            '@angular/material/core/theming/prebuilt/deeppurple-amber.css',
+            'ui-router-ng2/_bundles/ui-router-ng2.min.js',
+            'reflect-metadata/Reflect.js',
+            'zone.js/dist/zone.min.js',
+            'core-js/client/core.min.js',
+            'systemjs/dist/system.js'
+        ], { 'cwd': 'node_modules'})
+        .pipe(flatten())
+        .pipe(gulp.dest(outputDir.libraries));
     return merge(tmp1, tmp2);
-});
-// Copy the html files to dist/src/**/
-gulp.task('copy:html', false, () => {
-    const dest = 'dist/src';
-    return gulp.src('src/**/*.html')
-        .pipe(newer(dest))
-        .pipe(gulp.dest(dest));
-});
+}
 
-/////////////////////////
-/*      Build task     */
-/////////////////////////
-// Build the project
-gulp.task('build', 'Build the application', (cb) => {
-    run('clean', [
-        'compile:server',
-        'compile:js',
-        'compile:sass',
-        'copy:lib',
-        'copy:html'
-    ],  cb);
-});
-/////////////////////////
-/*        Watch        */
-/////////////////////////
+// Copy the html files to dist/src/**
+function copy_html() {
+    return (fs.existsSync(outputDir.client)
+        ? gulp.src('src/**/*.html', { 'since': startTime })
+        : gulp.src('src/**/*.html'))
+        .pipe(gulp.dest(outputDir.client));
+};
+
 // Watch for changes
-gulp.task('watch', false, (cb) => {
-    gulp.watch('src/**/*.scss', ['compile:sass'], cb);
-    gulp.watch('src/**/*.ts', ['compile:js'], cb);
-    gulp.watch('src/**/*.html', ['copy:html'], cb);
-    gulp.watch('server/**/*.ts', () => { run('compile:server', 'run'); }, cb);
-});
+function watch() {
+    const onchange = (path) => gutil.log(`Path ${chalk.green(path)} changed`);
+    gulp.watch('src/**/*.scss', gulp.series(compile_sass))
+        .on('change', onchange);
+    gulp.watch('src/**/*.ts', gulp.series(compile_js))
+        .on('change', onchange);
+    gulp.watch('src/**/*.html', gulp.series(copy_html))
+        .on('change', onchange);
+    gulp.watch('server/**/*.ts', gulp.series(compile_server, run_express))
+        .on('change', onchange);
+}
 
-/////////////////////////
-/*         Run         */
-/////////////////////////
-// Run the application
-gulp.task('run', 'Run the application and watch for changes', (cb) => {
-    return run('run:express', 'watch', cb);
-});
-// starts the server and handles process exit
-gulp.task('run:express', false, () => {
-    if (server === undefined) {
-        start();
-    } else {
-        restart();
+//////////////////////////
+/*    Express Server    */
+//////////////////////////
+// defines properties to be used by the ChildProcess instance
+class ServerWrapper {
+    constructor() {
+        throw new Error('ServerWrapper is meant to be used as a static class');
     }
 
-    // Event handlers
-    function stdout(data) {
-        gutil.log(chalk.green('[server] ') + chalk.blue(typeof data === 'string' ? data.trim() : data));
-    }
-    function stderr(data) {
-        gutil.log(chalk.green('[server] ') + chalk.red(typeof data === 'string' ? data.trim() : data));
-    }
-    function error(err) {
-        if (err instanceof Error)
-            throw err;
-        gutil.log(chalk.red(err));
-        kill();
-    }
-    function exit(code, sig) {
-        gutil.log(chalk.green('[server] ') +
-            'Exited with: ' +
-            chalk.red('[code => %d, sig => %s]'), code, sig);
-        removeEvent();
-        server = undefined;
-        //if the error code or sig is not null exit the process
-        if (code !== 0) process.exit(1);
-        // else restart the server
-        start();
-    }
-    // end Event handlers
-
-    function start() {
+    static start() {
         // create a child_process running the express server
-        server = spawn(
-            'node',
-            ['dist/server/wiki.js'],
-            {
+        this.server = fork('dist/server/wiki.js', {
                 'env': process.env,
-                'cwd': __dirname
-            }
-        );
-        setEvent();
+                'cwd': __dirname,
+                'silent': true
+        }).on('message', (data) => {
+            gutil.log(chalk.green('[server] ')
+                + chalk.blue(data));
+        }).on('exit', (code, sig) => {
+            gutil.log(chalk.green('[server] ')
+                + 'Exited with: [code => '
+                + chalk.red('%d')
+                + ', sig => '
+                + chalk.red('%s')
+                + ']', code, sig);
+            //if the error code > 0 or sig is not null exit the process
+            if (typeof code === 'number' && code !== 0) process.exit(1);
+        }).on('error', (err) => {
+            gutil.log(arguments);
+            gutil.log(chalk.red(err));
+            this.server = undefined;
+        });
     }
 
-    function restart() {
+    static restart() {
         // restart the child_process
-        kill();
-        gutil.log(chalk.green('[server] ') + chalk.blue('Restarting the server'));
-        start();
+        this.kill();
+        gutil.log(chalk.green('[server] ')
+            + chalk.blue('Restarting the server'));
+        this.start();
     }
 
-    function kill() {
+    static kill() {
         // kill the child process
-        if (server) {
-            server.kill('SIGTERM');
-            removeEvent();
-            server = undefined;
+        if (this.server) {
+            this.server.disconnect();
+            this.server.kill('SIGTERM');
+            this.server = undefined;
         }
     }
+}
 
-    function removeEvent() {
-        if (server) {
-            server.stdout.removeListener('data', stdout);
-            server.stderr.removeListener('err', stderr);
-            server.removeListener('exit', exit);
-            server.removeListener('error', error);
-        }
+run_express.description = 'Start the Express server';
+function run_express(cb) {
+    if (ServerWrapper.server === undefined) {
+        ServerWrapper.start();
+    } else {
+        ServerWrapper.restart();
     }
-
-    function setEvent() {
-        server.stdout.setEncoding('utf8');
-        server.stderr.setEncoding('utf8');
-        server.stdout.on('data', stdout);
-        server.stderr.on('data', stderr);
-        // when the server exist
-        server.once('exit', exit);
-        // when the server has a problem
-        server.once('error', error);
-    }
-});
+    cb();
+    return;
+};
