40ef85fd3be9cea2d20e04e5d3c72a1e4f5ae50a
Nicolas Berbiche <nic.berbiche@gmail.com>
2017-04-02 21:51:41 -0400

    Fixed the configuration of SystemJS

--- src/system.config.ts
+++ src/system.config.ts
@@ -1,34 +1,35 @@
-(function (global) {
-    let packages = {
-        'app': {
-            'defaultExtension': 'js'
-        }
-    }
-
-    let map = {
-        // our app is in the dist/src/app folder
-        'app': 'src/app',
-
-        '@angular/core': 'lib:core.umd.min.js',
-        '@angular/common': 'lib:common.umd.min.js',
-        '@angular/forms': 'lib:forms.umd.min.js',
-        '@angular/http': 'lib:http.umd.min.js',
-        '@angular/material': 'material.umd.js',
-        '@angular/platform-browser': 'lib:platform-browser.umd.min.js',
-        '@angular/platform-browser-dynamic': 'lib:platform-browser-dynamic.min.js',
-        '@angular/router': 'lib:router.min.js',
-        'ui-router-ng2': 'lib:ui-router-ng2.min.js',
-        'rxjs': 'lib:Rx.min.js'
-    }
-
-    ngPackageNames.forEach(setPackageConfig);
-
+(function (System: any): void {
     System.config({
-        'baseURL': '/dist',
-        'paths': {
-            'lib': 'lib'
+        'map': {
+            // our app is in the src/app folder
+            'app': 'src/app',
+            '@angular/core':                     'lib/core.umd.min.js',
+            '@angular/common':                   'lib/common.umd.min.js',
+            '@angular/compiler':                 'lib/compiler.umd.min.js',
+            '@angular/platform-browser':         'lib/platform-browser.umd.min.js',
+            '@angular/platform-browser-dynamic': 'lib/platform-browser-dynamic.umd.min.js',
+            '@angular/http':                     'lib/http.umd.min.js',
+            '@angular/router':                   'lib/router.min.js',
+            '@angular/forms':                    'lib/forms.umd.min.js',
+            '@angular/material':                 'lib/material.umd.js',
+            'ui-router-ng2':                     'lib/ui-router-ng2.min.js',
+            'rxjs':                              'lib/rxjs'
         },
-        'map': map,
-        'packages': packages
+        'packages': {
+            'app': {
+                'defaultExtension': 'js'
+            },
+            'wiki': {
+                'defaultExtension': 'js'
+            },
+            'rxjs': {
+                'defaultExtension': 'js'
+            }
+        },
+        'meta': {
+            '*.html': {
+                'loader': 'text'
+            }
+        }
     });
-})(this);
+})(System);
